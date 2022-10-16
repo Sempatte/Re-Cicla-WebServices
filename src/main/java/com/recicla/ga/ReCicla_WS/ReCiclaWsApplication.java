07178fea4823cc32765b6c0f61b2be3bc7c0266b
sempatte <sempatte@protonmail.com>
2022-10-16 16:50:42 -0500

    Project

---
+++ src/main/java/com/recicla/ga/ReCicla_WS/ReCiclaWsApplication.java
@@ -0,0 +1,13 @@
+package com.recicla.ga.ReCicla_WS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReCiclaWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReCiclaWsApplication.class, args);
+	}
+
+}
