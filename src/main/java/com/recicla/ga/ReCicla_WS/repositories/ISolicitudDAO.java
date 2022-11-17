e347dcef38e8cbce7e03a9e9d38f90a69d0a5980
Dacf18 <u201810646@upc.edu.pe>
2022-11-17 18:09:34 -0500

    SolicitudImplement
    implementacion del solicitud service

--- src/main/java/com/recicla/ga/ReCicla_WS/repositories/ISolicitudDAO.java
+++ src/main/java/com/recicla/ga/ReCicla_WS/repositories/ISolicitudDAO.java
@@ -1,10 +1,11 @@
 package com.recicla.ga.ReCicla_WS.repositories;
 
+import com.recicla.ga.ReCicla_WS.entities.Solicitud;
 import com.recicla.ga.ReCicla_WS.entities.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ISolicitudDAO extends JpaRepository<Ticket, Integer> {
+public interface ISolicitudDAO extends JpaRepository<Solicitud, Integer> {
 
 }
---
+++ src/main/java/com/recicla/ga/ReCicla_WS/serviceimpls/SolicitudServiceImpl.java
@@ -0,0 +1,53 @@
+package com.recicla.ga.ReCicla_WS.serviceimpls;
+
+import com.recicla.ga.ReCicla_WS.entities.Solicitud;
+import com.recicla.ga.ReCicla_WS.repositories.ISolicitudDAO;
+import com.recicla.ga.ReCicla_WS.services.ISolicitudService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class SolicitudServiceImpl implements ISolicitudService {
+    @Autowired
+    private ISolicitudDAO solicitudService;
+
+    @Override
+    @Transactional
+    public boolean Insert(Solicitud solicitud) {
+        Solicitud objSoli=solicitudService.save(solicitud);
+        if (objSoli==null){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    @Transactional
+    public void delete(int idSolicitud) {
+        solicitudService.deleteById(idSolicitud);
+    }
+
+    @Override
+    public List<Solicitud> list() {
+        return solicitudService.findAll();
+    }
+
+    @Override
+    public Optional<Solicitud> listarId(int idSolicitud) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Solicitud> buscarUsuarioSolicitud(String nombreUsuario) {
+        return null;
+    }
+
+    @Override
+    public Optional<Solicitud> findSolicitudByID(int idSolicitud) {
+        return Optional.of(solicitudService.findById(idSolicitud).orElse(new Solicitud()));
+    }
+}
