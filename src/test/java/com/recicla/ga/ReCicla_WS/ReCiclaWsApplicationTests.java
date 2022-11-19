c894fa53086eed454cc73e1b8687ef3b1279aa95
Sebastian De la Torre <sempatte@protonmail.com>
2022-11-18 21:17:55 -0500

    Security Backend

--- src/test/java/com/recicla/ga/ReCicla_WS/ReCiclaWsApplicationTests.java
+++ src/test/java/com/recicla/ga/ReCicla_WS/ReCiclaWsApplicationTests.java
@@ -1,13 +1,6 @@
 package com.recicla.ga.ReCicla_WS;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class ReCiclaWsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
