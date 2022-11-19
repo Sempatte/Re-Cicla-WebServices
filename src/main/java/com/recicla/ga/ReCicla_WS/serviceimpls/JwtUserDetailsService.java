68c53313003924d3c1da0c8d4e2adc552a99bcd8
Sebastian De la Torre <sempatte@protonmail.com>
2022-11-18 21:09:34 -0500

    Security Backend

---
+++ src/main/java/com/recicla/ga/ReCicla_WS/serviceimpls/JwtUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.recicla.ga.ReCicla_WS.serviceimpls;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //Aqui lógica para buscar el usuario en BD
+        //Usuario defecto web:password
+
+        if ("web".equals(username)) {
+            return new User("web", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+                    new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("Usuario no encontrado: " + username);
+        }
+    }
+}
