28d7266e71db72655c6dd00542d341b33a81d160
Sebastian De la Torre <sempatte@protonmail.com>
2022-11-18 23:25:25 -0500

    Security Backend

--- src/main/java/com/recicla/ga/ReCicla_WS/controllers/JwtAuthenticationController.java
+++ src/main/java/com/recicla/ga/ReCicla_WS/controllers/JwtAuthenticationController.java
@@ -40,7 +40,7 @@ public class JwtAuthenticationController {
 
         final String token = jwtTokenUtil.generateToken(userDetails);
 
-        return ResponseEntity.ok(new JwtResponse(token));
+        return ResponseEntity.ok(new JwtResponse(token) );
     }
 
     private void authenticate(String username, String password) throws Exception {
